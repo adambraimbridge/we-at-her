fb3034fbcd84a2586920dbb99aae1a50a90c9717
Adam Braimbridge <adam.braimbridge@ft.com>
2018-09-25 19:05:25 +0100

    readFileSync

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
 const PORT = process.env.PORT || 5000;
 const HOST = process.env.HOST || '0.0.0.0';
+
+const filePath = path.join(__dirname, 'index.html');
+const html = fs.readFileSync(filePath, 'utf8');
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
+  res.end(html);
 });
 server.listen(PORT, () => {
   console.log(`Server running on ${PORT}/`);
